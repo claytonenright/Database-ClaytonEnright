a207336c607be274b356418173d4ff0e101d80ed
Clayton Enright <clayton.enright@gmail.com>
2014-02-11 16:37:13 -0500

    Clayton Enright 2/14/14 Lab 4: The Sequel

---
+++ Lab4ClaytonEnright.sql
@@ -0,0 +1,80 @@
+﻿-- Lab 4: The Sequel
+-- Clayton Enright 2/14/14
+
+-- Query #1
+select city
+from agents
+where aid in (select aid
+	      from orders
+	      where cid in (select cid
+			    from customers
+			    where name = 'Basics'
+			    )
+	      )
+
+-- Query #2
+select distinct pid
+from orders
+where aid in (select aid
+	      from orders
+	      where cid in (select cid
+			    from customers
+			    where city = 'Kyoto'
+			    )
+	      )
+order by pid asc
+
+-- Query #3
+select cid, name
+from customers
+where cid not in (select cid
+		  from orders
+		  where aid = 'a03'
+		  )
+
+-- Query #4
+select cid, name
+from customers
+where cid in (select cid
+	      from orders
+	      where pid = 'p01'
+	      )
+  and cid in (select cid
+	      from orders
+	      where pid = 'p07'
+	      )
+
+-- Query #5
+select distinct pid
+from orders
+where cid in (select cid
+	      from orders
+	      where aid = 'a03'
+	      )
+order by pid
+
+-- Query #6
+select name, discount
+from customers
+where cid in (select cid
+	      from orders
+	      where aid in (select aid
+			    from agents
+			    where city = 'Dallas'
+			    or city = 'Duluth'
+			    )
+	      )
+
+-- Query #7
+select cid, name
+from customers
+where cid in (select cid
+	      from customers
+	      where discount in (select discount
+				  from customers
+				  where city = 'Dallas'
+				     or city = 'Kyoto'
+				  )
+	      )
+and city not in ('Dallas','Kyoto')
+
