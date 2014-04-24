242903d38df2050124b85c3a5458e77d69ae5577
Clayton Enright <clayton.enright@gmail.com>
2014-04-24 15:44:46 -0400

    SQL File for Lab 10 Stored Procedures Clayton Enright 4/25/14

---
+++ Lab10StoredProcedures.sql
@@ -0,0 +1,44 @@
+﻿-- Stored Procedure #1 --
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   course      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select prereqnum
+      from   prerequisites
+       where  courseNum = course;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select PreReqsFor(499, 'results');
+Fetch all from results;
+
+
+
+-- Stored Procedure #2 --
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   prereq      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select coursenum
+      from   prerequisites
+       where  prereqnum = prereq;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select IsPreReqFor(120, 'results');
+Fetch all from results;
+
+
+
+select *
+from prerequisites
